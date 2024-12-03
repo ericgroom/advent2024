9fc6c63a2d70f96bc2572178d75cfdc4b4d57260
Eric Groom <eric@gingerlabs.com>
2024-12-02 17:07:27 -0800

    day2 part1

--- Sources/advent2024/advent2024.swift
+++ Sources/advent2024/advent2024.swift
@@ -10,6 +10,8 @@ struct Advent2024 {
         Day1(input: getInput(filename: "day1")).part1()
         Day1(input: Day1.example).part2()
         Day1(input: getInput(filename: "day1")).part2()
+        Day2(input: Day2.example).part1()
+        Day2(input: getInput(filename: "day2")).part1()
     }
 
     static func getInput(filename: String) -> String {
---
+++ Sources/advent2024/days/day2.swift
@@ -0,0 +1,60 @@
+//
+//  day2.swift
+//  advent2024
+//
+//  Created by Eric Groom on 12/2/24.
+//
+
+import Foundation
+
+struct Day2 {
+    let input: String
+
+    static let example = """
+    7 6 4 2 1
+    1 2 7 8 9
+    9 7 6 2 1
+    1 3 2 4 5
+    8 6 4 4 1
+    1 3 6 7 9
+    """
+
+    func parse(_ input: String) -> [[Int]] {
+        input
+            .components(separatedBy: .newlines)
+            .filter { !$0.isEmpty }
+            .map { line in
+                line
+                    .components(separatedBy: .whitespaces)
+                    .compactMap { Int($0, radix: 10) }
+            }
+    }
+
+    // 332
+    func part1() {
+        let reports = parse(input)
+        let result = reports
+            .filter { safe(levels: $0) }
+            .count
+        print(result)
+    }
+
+    private func safe(levels: [Int]) -> Bool {
+        assert(levels.count >= 2)
+        var previousLevel = levels[0]
+        var increasing = false
+        var decreasing = false
+        for level in levels[1...] {
+            let prevIncreasing = increasing
+            let prevDecreasing = decreasing
+            defer { previousLevel = level }
+            let absDiff = abs(level - previousLevel)
+            guard 0 < absDiff && absDiff <= 3 else { return false }
+            increasing = level > previousLevel
+            decreasing = level < previousLevel
+            if prevIncreasing == true && increasing == false { return false }
+            if prevDecreasing == true && decreasing == false { return false }
+        }
+        return true
+    }
+}
