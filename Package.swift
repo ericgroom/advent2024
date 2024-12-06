93c705b061d62bed07d8d9e03d52658a7eeb2f5c
Eric Groom <eric@gingerlabs.com>
2024-12-06 11:01:08 -0800

    remove executable in favor of tests

--- Package.swift
+++ Package.swift
@@ -8,14 +8,15 @@ let package = Package(
     platforms: [.macOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-        .executable(name: "advent2024", targets: ["advent2024"])
+        .library(name: "advent2024", targets: ["advent2024"])
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(name: "advent2024",
-                          dependencies: ["InputFetcher"]
-                         ),
+        .target(
+            name: "advent2024",
+            dependencies: ["InputFetcher"]
+        ),
         .target(
             name: "InputFetcher",
             resources: [.process("secrets.json")]
